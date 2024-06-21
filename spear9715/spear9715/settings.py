c922155c3372d45431627d89baf4f118a8d9d3c6
Sean Pearce <72108425+spear97@users.noreply.github.com>
2024-06-21 14:43:24 -0500

    setting reconfig

--- spear9715/spear9715/settings.py
+++ spear9715/spear9715/settings.py
@@ -25,9 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'sentimentanalysis',
     'landing',
-    'aptdb'
 ]
 
 MIDDLEWARE = [
