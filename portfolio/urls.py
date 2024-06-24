a1da45bf09c765fb227e9046072db081b4a863f1
Sean Pearce <72108425+spear97@users.noreply.github.com>
2024-06-24 18:03:07 -0500

    implemented index view for aptdb app

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from landing import views as landing_views
+from aptdb import views as apt_views
 
 urlpatterns = [
     path('', landing_views.home, name='home'),
+    path('aptdb/', apt_views.index, name='aptdb'),
 ]
