ded2b6e39ab84c0382425038334747c4fcbd785e
Sean Pearce <72108425+spear97@users.noreply.github.com>
2024-06-23 21:54:13 -0500

    finished implementing landing app for portfolio website

--- landing/views.py
+++ landing/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home.html')
+
+    skill_names = [
+        'Python', 'Django', 'Flask', 'Amazon Web Services (AWS)', 'IBM Cloud',
+        'Artificial Intelligence (AI)', 'C#', 'C++', 'Cascading Style Sheets (CSS)',
+        'HTML', 'GitHub', 'Identity and Access Management (IAM)', 'PHP', 'Java',
+        'JavaScript', 'Linux', 'Object-Oriented Programming (OOP)', 'REST APIs',
+        'Unreal Engine', 'Unity'
+    ]
+
+    skills_percentage = [
+        80, 75, 70, 45, 50, 55, 45, 80, 55, 55, 65, 35, 50, 60, 55, 45, 70, 70, 80, 75
+    ]
+
+    skills = [{'name': name, 'percentage': percentage} for name, percentage in zip(skill_names, skills_percentage)]
+
+    return render(request, 'home.html', {'skills': skills})
 
 def about(request):
     return render(request, 'about.html')
