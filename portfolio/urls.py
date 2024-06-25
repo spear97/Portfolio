59f084d37a6635c20c7062c95bd4458851f59272
Sean Pearce <72108425+spear97@users.noreply.github.com>
2024-06-24 21:57:42 -0500

    update

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from landing import views as landing_views
 from aptdb import views as apt_views
+from sentimentanalysis import views as sent_views
 
 urlpatterns = [
     path('', landing_views.home, name='home'),
     path('aptdb/', apt_views.index, name='aptdb'),
     path('aptdb/get_apartments/', apt_views.search, name='aptdb_results'),
+    path('sentiment_analysis/', sent_views.index, name='sent_index'),
+    path('sentiment_analysis/analysis/', sent_views.sentiment_analysis, name='analysis')
 ]
