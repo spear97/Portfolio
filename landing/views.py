de23b90f8bc452b2c028139567e91d3a621ac430
Sean Pearce <72108425+spear97@users.noreply.github.com>
2024-08-25 19:14:22 -0500

    new portfolio project added

--- landing/views.py
+++ landing/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import json
 
 def home(request):
 
@@ -23,3 +24,59 @@ def about(request):
 
 def contact(request):
     return render(request, 'contact.html')
+
+"""Additional Portfolio Projects"""
+
+def currency_converter(request):
+
+    currency = {
+        'usd': {
+            'usd': 1.0,
+            'eur': 0.89,
+            'gbp': 0.76,
+            'jpy': 144.01
+        },
+        'eur': {
+            'usd': 1.12,
+            'eur': 1.0,
+            'gbp': 0.85,
+            'jpy': 161.13
+        },
+        'gbp': {
+            'usd': 1.32,
+            'eur': 1.18,
+            'gbp': 1.0,
+            'jpy': 190.26
+        },
+        'jpy': {
+            'usd': 0.0069,
+            'eur': 0.0062,
+            'gbp': 0.0053,
+            'jpy': 1.0
+        }
+    }
+
+    currency_list = list(currency.keys())
+
+
+    if request.method == 'POST':
+
+        currency1 = request.POST.get('currency1')
+        currency2 = request.POST.get('currency2')
+
+        weight = currency[currency1][currency2]
+
+        result = f"1.0 {currency1} = {weight} {currency2}"
+
+        content = {
+            'currency_list': json.dumps(currency_list),
+            'result': json.dumps(result)
+        }
+
+        return render(request, 'currency_converter_base.html', content)
+
+    content = {
+        'currency_list': json.dumps(currency_list),
+    }
+
+    return render(request, 'currency_converter_base.html', content)
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', landing_views.home, name='home'),
     path('aptdb/', apt_views.index, name='aptdb'),
     path('aptdb/get_apartments/', apt_views.search, name='aptdb_results'),
+    path('currency_converter/', landing_views.currency_converter, name='currency_converter')
 ]
